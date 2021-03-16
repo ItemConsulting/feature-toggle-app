b28a597e406d4f5c5808ce20f0d90397098ebe80
Garlov <oe.nordli@gmail.com>
2021-03-16 14:05:31 +0100

    selector with spaces

--- src/main/resources/react4xp/entries/containers/Main/index.jsx
+++ src/main/resources/react4xp/entries/containers/Main/index.jsx
@@ -1,5 +1,5 @@
-import { Button, Container, LinearProgress, makeStyles, Typography } from '@material-ui/core';
-import React, { useEffect } from 'react';
+import { Button, Container, FormControl, InputLabel, LinearProgress, makeStyles, MenuItem, Select, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectSpaces, selectSpacesStatus, selectSpacesError, selectGetSpacesUrl } from './selectors';
 import { requestSpaces } from './actions';
@@ -9,6 +9,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(2),
   },
+  formControll: {
+    minWidth: 120
+  }
 }));
 
 export function Main() {
@@ -19,12 +22,29 @@ export function Main() {
   const spaces = useSelector(selectSpaces);
   const spacesStatus = useSelector(selectSpacesStatus);
   const spacesError = useSelector(selectSpacesError);
+  const [space, setSpace] = useState('');
 
   useEffect(() => {
     if (spacesStatus === 'idle' && getSpacesUrl) {
       requestSpaces(dispatch, getSpacesUrl);
     }
   });
+
+  function renderSpaceForm() {
+    return (
+      <FormControl className={classes.formControll}>
+        <InputLabel id="space-select-label">Space</InputLabel>
+        <Select labelId="space-select-label" id="space-select" value={space} onChange={(e) => setSpace(e.target.value)}>
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {spaces.map((s) => {
+            return <MenuItem key={s._id} value={s._name}>{s._name}</MenuItem>;
+          })}
+        </Select>
+      </FormControl>
+    );
+  }
 
   function renderSpaces() {
     if (spacesStatus === 'loading' || spacesStatus === 'idle') {
@@ -33,11 +53,9 @@ export function Main() {
       return <Alert severity="error">{spacesError}</Alert>;
     } else if (spacesStatus === 'succeeded') {
       if (spaces.length > 0) {
-        return spaces.map((space) => {
-          return <div key={space._id}>{space._name}</div>;
-        });
+        return renderSpaceForm();
       } else {
-        return <Alert severity="info">No spaces</Alert>
+        return <Alert severity="info">No spaces</Alert>;
       }
     }
   }
