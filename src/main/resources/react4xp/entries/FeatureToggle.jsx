d87f15e6f1ed7e28242f3102b82d11872188e0cb
Anne-Siri Trønnes <ntr@ssb.no>
2023-08-30 15:39:39 +0200

    fixed breaking change mui and react-router-dom

--- src/main/resources/react4xp/entries/FeatureToggle.jsx
+++ src/main/resources/react4xp/entries/FeatureToggle.jsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { Provider, useDispatch } from 'react-redux';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { configureAppStore } from './store/configureStore';
 
 import { Main } from './containers/Main/index';
 import { Header } from './containers/Header/index';
 
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import { CssBaseline, responsiveFontSizes } from '@mui/material';
+import { createTheme, ThemeProvider, styled, responsiveFontSizes } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { setServiceUrls } from './containers/Main/actions';
 
 const PREFIX = 'FeatureToggle';
@@ -47,9 +47,9 @@ function FeatureToggle(props) {
             <CssBaseline />
             <div className={classes.root}>
               <Header />
-              <Switch>
+              <Routes>
                 <Route path="/" component={Main} />
-              </Switch>
+              </Routes>
             </div>
           </ThemeProvider>
         </BrowserRouter>
